76c946740378c602aab6df13a9736f65c395083d
Faraenju02 <fabianracini@hotmail.com>
2023-04-12 21:42:18 -0500

    version 1.1 swagger documentaion

--- src/app.js
+++ src/app.js
@@ -1,10 +1,31 @@
 import express from "express";
-import  morgan  from "morgan";
+import morgan from "morgan";
+import path from "path"
+
+
+//swagger
+import swaggerUi from "swagger-ui-express"
+import swaggerJSDoc from "swagger-jsdoc";
 
 //Route
 import contactRoute from "./routes/contact.route"
 
 const app = express()
+const swaggerSpec = {
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "node Mysql api",
+            version: "1.0.0"
+        },
+        servers: [
+            {
+                url: "http://localhost:3000"
+            }
+        ]
+    },
+    apis:[`${path.join(__dirname,"./routes/contact.route.js")}`]
+}
 
 //setting
 app.set('port', 3000)
@@ -12,8 +33,9 @@ app.set('port', 3000)
 //middleware
 app.use(morgan("dev"))
 app.use(express.json())
+app.use("/api-doc", swaggerUi.serve, swaggerUi.setup(swaggerJSDoc(swaggerSpec)))
 
 //exec route
-app.use("/api/contacts",contactRoute)
+app.use("/api/contacts", contactRoute)
 
 export default app
--- src/routes/contact.route.js
+++ src/routes/contact.route.js
@@ -3,11 +3,179 @@ import { methods as contactController } from "../controllers/contact.controller"
 
 const router = Router()
 
+//create contact
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *      Users:
+ *        type: object
+ *        properties:
+ *          name:
+ *            type: string
+ *            description: the user name       
+ *          age:
+ *            type: integer
+ *            description: the user age
+ *          phone:
+ *             type: string
+ *             description: the user phone
+ *          email:
+ *             type: string
+ *             description: the user email
+ *        required: 
+ *          - name
+ *          - age
+ *          - phone
+ *          - email 
+ *        example:
+ *          name: javier gozalez
+ *          age: 24
+ *          phone: 532273
+ *          email: examplea.com
+ * 
+ */
+
+////////////////////////////////////////////////////
+
+// get all contacts
+/**
+ * @swagger
+ * /api/contacts:
+ *  get:
+ *      summary: Return all contacts
+ *      tags: [Users]    
+ *      responses:
+ *          200:
+ *              description: All contacts
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: array
+ *                          items:
+ *                              $ref: '#/components/schemas/Users'
+ *                          
+ *                  
+ *                  
+ */
 
 router.get("/", contactController.getContacts)
+
+// get contact
+/**
+ * @swagger
+ * /api/contacts/{id}:
+ *  get:
+ *      summary: Return contact
+ *      tags: [Users] 
+ *      parameters:
+ *          - in: path
+ *            name: id
+ *            schema:
+ *              type: integer
+ *            require: true
+ *            description: the user id
+ *      responses:
+ *          200:
+ *              description: All contacts
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: object
+ *                          $ref: '#/components/schemas/Users'
+ *          404:
+ *              description: Contact not found
+ *                          
+ *                  
+ *                  
+ */
+
 router.get("/:id", contactController.getContact)
+
+// Add contact
+/**
+ * @swagger
+ * /api/contacts:
+ *  post:
+ *      summary: create new contact
+ *      tags: [Users]
+ *      requestBody:
+ *          required: true
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      type: object
+ *                      $ref: '#/components/schemas/Users'
+ *      responses:
+ *          200:
+ *              description: New contact created
+ *                  
+ */
+
 router.post("/", contactController.addContact)
+
+// Delete contact
+/**
+ * @swagger
+ * /api/contacts/{id}:
+ *  delete:
+ *      summary: Delete contact
+ *      tags: [Users] 
+ *      parameters:
+ *          - in: path
+ *            name: id
+ *            schema:
+ *              type: integer
+ *            require: true
+ *            description: the user id
+ *      responses:
+ *          200:
+ *              description: Contact Deleted
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: object
+ *                          $ref: '#/components/schemas/Users'
+ *          404:
+ *              description: Contact not found
+ *                          
+ *                  
+ *                  
+ */
 router.delete("/:id", contactController.deleteContact)
+
+// Update contact
+/**
+ * @swagger
+ * /api/contacts/{id}:
+ *  put:
+ *      summary: Update contact
+ *      tags: [Users] 
+ *      parameters:
+ *          - in: path
+ *            name: id
+ *            schema:
+ *              type: integer
+ *            require: true
+ *            description: the user id
+ * 
+ *      requestBody:
+ *          required: true
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      type: object
+ *                      $ref: '#/components/schemas/Users'
+ *      responses:
+ *          200:
+ *              description: Contact Updated
+ *              
+ *          404:
+ *              description: Contact not found
+ *                          
+ *                  
+ *                  
+ */
 router.put("/:id", contactController.updateContact)
 
 export default router
